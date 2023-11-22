0825366ccc0b9b847e4256de6fec13a8b9220738
ZyMa-1 <57665203+ZyMa-1@users.noreply.github.com>
2023-11-23 02:29:37 +0600

    Outline of loggingstats

--- src/core/PathManager.py
+++ src/core/PathManager.py
@@ -3,13 +3,16 @@ import pathlib
 
 class PathManager:
     PROJECT_ROOT: pathlib.Path | None
-    DATA_DIR = pathlib.Path | None
-    TEMP_DIR = pathlib.Path | None
+    DATA_DIR: pathlib.Path | None
+    TEMP_DIR: pathlib.Path | None
 
     TRUSTED_USERS_PATH: pathlib.Path | None
     ADMIN_USERS_PATH: pathlib.Path | None
     DISCORD_USERS_DATA_DB_PATH: pathlib.Path | None
     DOT_ENV_PATH: pathlib.Path | None
+
+    LOGS_DIR: pathlib.Path | None
+    OSU_API_MAIN_LOG_PATH: pathlib.Path | None
 
     @classmethod
     def set_project_root(cls, project_root: pathlib.Path):
@@ -21,3 +24,6 @@ class PathManager:
         cls.ADMIN_USERS_PATH = cls.PROJECT_ROOT / "data" / "admins.json"
         cls.DISCORD_USERS_DATA_DB_PATH = cls.PROJECT_ROOT / "data" / "discord_users_data.db"
         cls.DOT_ENV_PATH = cls.PROJECT_ROOT / ".env"
+
+        cls.LOGS_DIR = cls.PROJECT_ROOT / "logs"
+        cls.OSU_API_MAIN_LOG_PATH = cls.PROJECT_ROOT / "logs" / "osu_api" / "osu_api.log"
---
+++ src/my_logging/LoggingStats.py
@@ -0,0 +1,54 @@
+import datetime
+import re
+from typing import List, Optional
+
+import aiofiles
+
+from core import PathManager
+
+
+class LoggingStats:
+    @staticmethod
+    async def calculate_stats(time_periods: Optional[List[int]] = None):
+        if time_periods is None:
+            time_periods = [60, 300, 3600, 24 * 3600]
+
+        stats = {}
+        for time_period_seconds in time_periods:
+            tokens_spent = await LoggingStats.calculate_tokens_spent(time_period_seconds)
+            stats[f'{time_period_seconds // 60}min'] = {
+                'tokens_spent': tokens_spent,
+            }
+
+        return stats
+
+    @staticmethod
+    async def calculate_tokens_spent(time_period_seconds: int) -> int:
+        if time_period_seconds < 0 or time_period_seconds > 3600 * 24:
+            raise RuntimeError("Function does not designed to work on that time_period yet.")
+
+        async with aiofiles.open(PathManager.OSU_API_MAIN_LOG_PATH, "r") as file:
+            logs = await file.readlines()
+
+        log_pattern = re.compile(
+            r'(?P<timestamp>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).*tokens_spent=(?P<tokens_spent>\d+)')
+
+        total_tokens_spent = 0
+        current_time = datetime.datetime.now()
+
+        for log_entry in reversed(logs):
+            match = log_pattern.search(log_entry)
+            if match:
+                timestamp_str = match.group('timestamp')
+                tokens_spent = int(match.group('tokens_spent'))
+
+                # Parse the timestamp
+                timestamp = datetime.datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S')
+
+                # Check If timestamp is within the time period
+                if (current_time - timestamp).total_seconds() <= time_period_seconds:
+                    total_tokens_spent += tokens_spent
+                else:
+                    break
+
+        return total_tokens_spent
