afaa563c2864b66a5074b769832c643f610bec24
ZyMa-1 <57665203+ZyMa-1@users.noreply.github.com>
2023-11-23 02:41:31 +0600

    New cog and some minor doc change

---
+++ src/discord_extension_stuff/cogs/LoggingStatsCog.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+from discord.ext.commands import Context
+
+from core import BotContext
+from my_logging.LoggingStats import LoggingStats
+
+
+class LoggingStatsCog(commands.Cog):
+    def __init__(self, bot_context: BotContext):
+        self.bot = bot_context.bot
+        self.logging_stats = LoggingStats()
+
+    @commands.command(name='logging_stats')
+    async def logging_stats_command(self, ctx: Context):
+        """
+        Logging stats on osi! api v2 calls via 'ossapi' python wrapper.
+        """
+        response = await self.logging_stats.calculate_stats()
+        await ctx.send(response)
--- src/discord_extension_stuff/cogs/__init__.py
+++ src/discord_extension_stuff/cogs/__init__.py
@@ -3,3 +3,4 @@ from .LogicCog import LogicCog
 from .OsuApiLogicCog import OsuApiLogicCog
 from .PersonalMessageCog import PersonalMessageCog
 from .SetupCog import SetupCog
+from .LoggingStatsCog import LoggingStatsCog
