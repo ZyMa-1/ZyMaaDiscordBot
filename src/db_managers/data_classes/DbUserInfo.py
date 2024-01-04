3ab3e1d39f2b2550010962630f0d80e16afc5b4d
ZyMa-1 <misha01052004@gmail.com>
2024-01-04 22:05:44 +0600

    Dumb fix

--- src/db_managers/data_classes/DbUserInfo.py
+++ src/db_managers/data_classes/DbUserInfo.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from functools import cache
 from typing import TYPE_CHECKING, Optional
 
 from ossapi import GameMode
@@ -26,7 +25,6 @@ class DbUserInfo:
                    osu_user_id=row.osu_user_id,
                    osu_game_mode=row.osu_game_mode)
 
-    @cache
     async def osu_user_name(self) -> Optional[str]:
         osu_api_utils = UtilsFactory.get_osu_api_utils()
         user = await osu_api_utils.get_user(self.osu_user_id)
